b5548b337c4bf65225710d6dfe3e5b544133b0d0
TenzingS <thesherpatenzing@gmail.com>
2022-05-18 18:00:07 -0400

    barebones frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="text-3xl font-bold underline text-slate-500">
       Group Stock App
     </div>
   );
